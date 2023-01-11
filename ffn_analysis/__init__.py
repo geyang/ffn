cd04b692e1495d665f512b3a918a3f1670c10819
Ge Yang <yangge1987@gmail.com>
2023-01-10 20:00:41 -0500

    add docker image

--- ffn_analysis/__init__.py
+++ ffn_analysis/__init__.py
@@ -11,13 +11,3 @@ RUN.prefix = "{project}/{project}/{username}/{now:%Y/%m-%d}/{file_stem}/{job_nam
 RUN.script_root = dirname(__file__)  # specify that this is the script root.
 print(colored('set', 'blue'), colored("RUN.script_root", "yellow"), colored('to', 'blue'),
       RUN.script_root)
-
-AWS_REGIONS = [
-    "ap-northeast-1", "ap-northeast-2", "ap-south-1", "ap-southeast-1",
-    "ap-southeast-2", "eu-central-1", "eu-west-1", "sa-east-1", "us-east-1",
-    "us-east-2", "us-west-1", "us-west-2", ]
-
-import yaml
-
-with open(join(dirname(__file__), "../ec2_setup/ec2_image_ids.yml"), 'r') as stream:
-    IMAGE_IDS = yaml.load(stream, Loader=yaml.BaseLoader)
